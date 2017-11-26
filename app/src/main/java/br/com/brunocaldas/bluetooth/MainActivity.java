7aaf0cae747620f854184b1225d321b14e5a1d3c
Bruno Caldas <brunopaivacaldas@hotmail.com>
2017-11-26 20:50:25 -0200

    Ativação de bluetooth com descoberta

--- app/src/main/java/br/com/brunocaldas/bluetooth/MainActivity.java
+++ app/src/main/java/br/com/brunocaldas/bluetooth/MainActivity.java
@@ -7,7 +7,10 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -15,8 +18,10 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity {
 
     ToggleButton ligar;
+    Button visibilidade;
     BluetoothAdapter bluetoothAdapter;
     static final int HABILITA_BT = 1;
+    static final int HABILITA_DESCOBERTA = 2;
 
     private TextView mTextMessage;
 
@@ -53,7 +58,11 @@ public class MainActivity extends AppCompatActivity {
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
-            Toast.makeText(getApplicationContext(),"Não foi encontrado nenhum adaptador Bluetooth no aparelho", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Não foi encontrado nenhum adaptador Bluetooth no aparelho", Toast.LENGTH_LONG).show();
+        }
+
+        if (bluetoothAdapter.isEnabled()) {
+            ligar.setChecked(true);
         }
 
         ligar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -65,15 +74,52 @@ public class MainActivity extends AppCompatActivity {
                     }
                 } else {
                     if (bluetoothAdapter.isEnabled()) {
-                        bluetoothAdapter.disable();
+                        if (bluetoothAdapter.disable()) {
+                            Toast.makeText(getApplicationContext(), "Bluetooth Desligado!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Falha ao desligar o Bluetooth!", Toast.LENGTH_SHORT).show();
+                            ligar.setChecked(true);
+                        }
                     }
                 }
+            }
+        });
+
+        visibilidade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                startActivityForResult(bluetoothIntent, HABILITA_DESCOBERTA);
+                ligar.setChecked(true);
             }
         });
     }
 
     private void binding() {
         ligar = (ToggleButton) findViewById(R.id.btnLigar);
+        visibilidade = (Button) findViewById(R.id.btnVisibilidade);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == HABILITA_BT) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Bluetooth Ligado!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Falha ao ativar o Bluetooth!", Toast.LENGTH_SHORT).show();
+                ligar.setChecked(false);
+            }
+        }
+
+        if (requestCode == HABILITA_DESCOBERTA) {
+            if (resultCode > 0) {
+                Toast.makeText(getApplicationContext(), "O Bluetooth está visível por " + resultCode + " segundos.", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Falha ao tornar o Bluetooth visível!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
